4b4c458791c7ee7ee1bcb0ac699bb2654281a303
Efrat Rosmarin <efratedit@gmail.com>
2018-05-08 19:28:43 -0400

    partially working

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,14 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection;
 
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-	var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-})
-}
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burgers_db"
+});
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
